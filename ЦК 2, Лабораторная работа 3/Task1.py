96023c63b8ccc9863009ded21a5bb6353eb68ed7
ToropovPF <115175252+ToropovPF@users.noreply.github.com>
2023-01-29 23:56:51 +0300

    Update Task1

--- ЦК 2, Лабораторная работа 3/Task1.py
+++ ЦК 2, Лабораторная работа 3/Task1.py
@@ -17,7 +17,7 @@ class Book:
         self._author = author
 
     def __str__(self):
-        '''
+        """
         Строковый метод класса Book
         :return: возвращает название книги и её автора
 
@@ -25,42 +25,42 @@ class Book:
         >>> book = Book('Капитанская дочка', 'А.С Пушкин')
         >>> print(book)
         Книга Капитанская дочка. Автор А.С Пушкин
-        '''
+        """
         return f"Книга {self.name}. Автор {self.author}"
 
     def __repr__(self):
-        '''
+        """
         Еще один строковый метод, но который возвращает валидную питоновскую строку
         :return: Питоновская строка, которой можно инициализировать объект
         Примеры:
         >>> book = Book('Капитанская дочка', 'А.С Пушкин')
         >>> print(repr(book))
         Book(name='Капитанская дочка', author='А.С Пушкин')
-        '''
+        """
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
     @property
     def name(self) -> str:
-        '''
+        """
         Делает приватный атрибут _name свойством name.
         :return: Возвращает значение свойства name
         Примеры:
         >>> book = Book('Капитанская дочка', 'А.С Пушкин')
         >>> print(book.name)
         Капитанская дочка
-        '''
+        """
         return self._name
 
     @property
     def author(self) -> str:
-        '''
+        """
         Делает приватный атрибут _author свойством author
         :return: Возвращает значение для свойства author
         Примеры:
         >>> book = Book('Капитанская дочка', 'А.С Пушкин')
         >>> print(book.author)
         А.С Пушкин
-        '''
+        """
         return self._author
 
 
@@ -75,34 +75,34 @@ class PaperBook(Book):
         :param pages: кол-во страниц
         """
         super().__init__(name, author)
-        self._pages = pages
+        self.pages = pages
 
     def __repr__(self):
-        '''
+        """
         Cтроковый метод, но который возвращает валидную питоновскую строку
         :return: Питоновская строка, которой можно инициализировать объект
         Примеры:
         >>> book = PaperBook('Капитанская дочка', 'А.С Пушкин', 300)
         >>> print(repr(book))
         PaperBook(name='Капитанская дочка', author='А.С Пушкин', pages=300)
-        '''
+        """
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
     @property
     def pages(self) -> int:
-        '''
+        """
         Делает приватный атрибут _pages свойством pages
         :return: Возвращает значение свойства pages
         Примеры
         >>> book = PaperBook('Капитанская дочка', 'А.С Пушкин', 300)
         >>> print(book.pages)
         300
-        '''
+        """
         return self._pages
 
     @pages.setter
     def pages(self, pages: int) -> None:
-        '''
+        """
         Сэттер для свойства pages
         :param pages: Новое значение для свойства pages
         :return: Ничего не возвращает. Присваивает новое значение для свойства pages
@@ -113,7 +113,7 @@ class PaperBook(Book):
         >>> book.pages = -1
         ValueError: Количество страниц не может быть отрицательным
         >>> book.pages = 300
-        '''
+        """
         if not isinstance(pages, int):
             raise TypeError("Количество страниц - целое число")
         if pages < 0:
@@ -125,43 +125,43 @@ class AudioBook(Book):
     """ Дочерний класс. Аудиокнига. """
 
     def __init__(self, name: str, author: str, duration: float):
-        '''
+        """
         Создание и подготовка к работе объекта AudioBook
         :param name: Название книги
         :param author: Автор книги
         :param duration: Продолжительность аудиокниги
         Примеры:
         >>> book = AudioBook('Капитанская дочка', 'А.С. Пушкин', 201.1)
-        '''
+        """
         super().__init__(name, author)
-        self._duration = duration
+        self.duration = duration
 
     def __repr__(self):
-        '''
+        """
         Cтроковый метод, но который возвращает валидную питоновскую строку
         :return: Питоновская строка, которой можно инициализировать объект
         Примеры:
         >>> book = AudioBook('Капитанская дочка', 'А.С Пушкин', 200.1)
         >>> print(repr(book))
         AudioBook(name='Капитанская дочка', author='А.С Пушкин', duration=200.1)
-        '''
+        """
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
 
     @property
     def duration(self) -> float:
-        '''
+        """
         Делает атрибут _duration свойством
         :return: Возвращает значене атрибута duration
         Примеры:
         >>> book = AudioBook('Капитанская дочка', 'А.С Пушкин', 200.1)
         >>> print(book.duration)
         200.1
-        '''
+        """
         return self._duration
 
     @duration.setter
     def duration(self, duration: float) -> None:
-        '''
+        """
         Сэттер свойства duration
         :param duration: Новое значение для свойства duration
         :return: Ничего не возвращает. Присваивает новое значение для свойства duration
@@ -171,7 +171,7 @@ class AudioBook(Book):
         TypeError: Длительность должна быть типа float
         >>> book.duration = -200
         ValueError: Длительность аудиокниги не может быть отрицательной
-        '''
+        """
         if not isinstance(duration, float):
             raise TypeError("Длительность аудиокниги должна быть типа float")
         if duration <= 0:
@@ -181,9 +181,9 @@ class AudioBook(Book):
 
 if __name__ == '__main__':
     doctest.testmod()
-    # book = PaperBook('Капитанская дочка', 'А.С Пушкин', 300)
-    # print(book)
-    # print(repr(book))
-    # ebook = AudioBook('Капитанская дочка', 'А.С Пушкин', 200.1)
-    # print(ebook.duration)
-    # print(repr(ebook))
+    book = PaperBook('Капитанская дочка', 'А.С Пушкин', 100)
+    print(book)
+    print(repr(book))
+    ebook = AudioBook('Капитанская дочка', 'А.С Пушкин', 200.1)
+    print(ebook.duration)
+    print(repr(ebook))
